e7fd98318b906582d838d45d3ce94064c49c4ca6
guochenghao <150vb@163.com>
2018-09-22 13:22:38 +0800

    打包2.2.2版本

--- MapxusMapSDK/MapxusMapSDK.h
+++ MapxusMapSDK/MapxusMapSDK.h
@@ -17,6 +17,7 @@ FOUNDATION_EXPORT const unsigned char MapxusMapSDKVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <MapxusMapSDK/PublicHeader.h>
 
 #import "MXMMapServices.h"
+
 #import "MapxusMap.h"
 #import "MapxusMapDelegate.h"
 #import "MXMGeoBuilding.h"
