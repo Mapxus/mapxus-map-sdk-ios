6cd68a357b0516cacc9a6e322be41accc71abab3
chenghao guo <q150vb@qq.com>
2023-01-11 09:30:08 +0800

    [SDK-475] 【iOS SDK】检查SDK访问的所有API是否为正确域名“map-api.mapxus.com”并更新token API域名

--- MapxusMapSDK/FoundationKit/MXMConstants.h
+++ MapxusMapSDK/FoundationKit/MXMConstants.h
@@ -9,4 +9,3 @@
 #import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT NSString *const MXMAPIHOSTURL; // api host address
-FOUNDATION_EXPORT NSString *const MXMTOKENHOSTURL; // get token host address
