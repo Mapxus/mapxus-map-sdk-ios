2de7820faaaa3a201f9ebc0967596c26769130c6
guochenghao <chenghaoguo@mapxus.com>
2024-09-26 14:27:46 +0800

    更新document.sh文件

--- document.sh
+++ document.sh
@@ -6,7 +6,12 @@
 #  Created by Chenghao Guo on 2018/7/31.
 #  Copyright © 2018年 MAPHIVE TECHNOLOGY LIMITED. All rights reserved.
 
-# Xcode script
+###### 文档生成方式
+# 1. 先使用AutoBuild脚本对项目打包一次
+# 2. 把MapxusMapSDK.h文件中的尖括号引用全改成双引号引用，如#import <MapxusMapSDK/MXMDefine.h>改成#import "MXMDefine.h"
+######
+
+
 # Start constants
 company="Maphive Technology Investment Limited.";
 companyURL="http://www.mapxus.com/";
@@ -25,40 +30,9 @@ jazzy \
 --author "${company}" \
 --author_url "${companyURL}" \
 --module MapxusMapSDK \
---module-version 6.8.0 \
+--module-version 6.11.0 \
 --readme README.md \
---hide-declarations swift \
 --disable-search \
 --skip-undocumented \
 --hide-documentation-coverage \
 --exclude MapxusMapSDK/Private,MapxusMapSDK/FoundationKit,MapxusMapSDK/Operations,MapxusMapSDK/Controllers/KxMenu
-
-
-##appledoc Xcode script
-## Start constants
-#company="Maphive Technology Investment Limited.";
-#companyURL="http://www.mapxus.com/";
-#target="iphoneos";
-#outputPath="${PROJECT_DIR}/Document";
-## End constants
-#
-#appledoc \
-#--project-name "${PROJECT_NAME}" \
-#--project-company "${company}" \
-#--output "${outputPath}" \
-#--logformat xcode \
-#--create-html \
-#--no-create-docset \
-#--no-install-docset \
-#--no-publish-docset \
-#--keep-intermediate-files \
-#--no-repeat-first-par \
-#--no-warn-invalid-crossref \
-#--exit-threshold 2 \
-#-i MapxusMapSDK/Private \
-#-i MapxusMapSDK/FoundationKit \
-#-i MapxusMapSDK/Operations \
-#-i MapxusMapSDK/Controllers/KxMenu \
-#-i MapxusMapSDK/Controllers/MXMPicker \
-#-i MapxusMapSDK/Controllers/MXMFloorSelectorBar.m \
-#"${PROJECT_DIR}/MapxusMapSDK"
