af4454545e8b09983bcdd851815f7d2c6d8baf1d
guochenghao <chenghaoguo@mapxus.com>
2024-09-27 15:12:51 +0800

    # CHANGELOG
    ## v6.11.0 (2024-09-27)
    
    ### 🎉 New
    
    * Added a method `- [MXMCategorySearch searchPoiCategoriesInBoundingBox:]` to query all POI categories within a specified bounding box that contain specified keywords.
    
    * Added support for fil, id, pt, th, vi, and ar languages in the `MXMultilingualObject` class.
    
    ### 📝 Changes
    
    * Replaced `- [MXMSearchAPI MXMPOICategorySearch:]` with `- [MXMCategorySearch searchPoiCategoriesByFloor:]`, `- [MXMCategorySearch searchPoiCategoriesByBuilding:]`, and `- [MXMCategorySearch searchPoiCategoriesByVenue:]` for indoor POI category queries.
    
    ### ❌ will be deleted soon
    
    * `MapxusMap.floorSwitchMode` will be removed, and always switch according to the venue.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "6.10.0"
+let version = "6.11.0"
 
 let package = Package(
   name: "MapxusMapSDK",
@@ -19,7 +19,7 @@ let package = Package(
     .binaryTarget(
       name: "MapxusMapSDK",
       url: "https://nexus3.mapxus.com/repository/ios-sdk/\(version)/mapxus-map-sdk-ios.zip",
-      checksum: "56f04b9b4b5016e58131830648b81f71329104628f3c57e0fbf17955cce237d8"
+      checksum: "f8c2bcc212380fe51704764bbb0b393de85fc31eadbe26bdaa2dd3415da62e2a"
     )
   ]
 )
