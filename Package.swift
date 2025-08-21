6d88ba464c7d269570a7a8f4a4853425dc538549
chenjiajie <jiajiechen@mapxus.com>
2025-08-21 15:03:40 +0800

    # CHANGELOG
    ## v8.2.0 (2025-08-21)
    
    ### 🎉 New
    
    * Added 5 new theme styles.
    * Added 2 new display section related styles.
    
    ### 📝 Changes
    
    * User can search POI with over 1 floor Ids or shared floor Ids
    
    ### 🐛  Fixes
    
    * Fixed bug where didSingleTapOnBlank and didLongPressAt returned non-nil MXMSite when user tapped on outdoor map area.
    
    ### ❌ will be deleted soon
    
    * Deprecated 4 old theme styles

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "8.1.0"
+let version = "8.2.0"
 
 let package = Package(
   name: "MapxusMapSDK",
@@ -19,7 +19,7 @@ let package = Package(
     .binaryTarget(
       name: "MapxusMapSDK",
       url: "https://nexus3.mapxus.com/repository/ios-sdk/\(version)/mapxus-map-sdk-ios.zip",
-      checksum: "42b64c463da5463b5df85548c2fc29d3075aa3d32a546223acdc6c0a11e9cbc8"
+      checksum: "1221a2a3d2aa0c65f01f091a076c71e1d79faa310eb8de0e593947f8ac4a73f1"
     )
   ]
 )
